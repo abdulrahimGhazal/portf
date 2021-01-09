bda8e889239f6da79d93e39074b7efebdb6e2a99
Abd <gazal@group-ib.com>
2021-01-09 23:39:17 +0300

    ffo

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # return render_template("main.html")
-    return 'Hello World!'
+    return "Hello World!\nI'm Great"
 
     
 if __name__ == "__main__":
