c94a6983f045ed16a253d8e3868469a2a00c9568
Abd <gazal@group-ib.com>
2021-01-09 23:03:19 +0300

    hh

--- hello.py
+++ hello.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("main.html")
-    
-@app.route("/about)
-def about():
-    return render_template("about.html")
+
     
 if __name__ == "__main__":
     app.run(debug=True)
